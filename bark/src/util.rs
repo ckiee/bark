6993cc886692f728047dc4660d3faae775d8883e
ckie <git-525ff67@ckie.dev>
2024-04-14 20:03:29 +0300

    bla bla ptrs

--- bark/src/util.rs
+++ bark/src/util.rs
@@ -6,9 +6,6 @@ use crate::RunError;
 pub const SAMPLE_FORMAT: SampleFormat = SampleFormat::F32;
 
 pub fn make_stream_config(configs: Box<dyn Iterator<Item = SupportedStreamConfigRange>>) -> Result<StreamConfig, RunError> {
-    let configs = device.supported_input_configs()
-        .map_err(RunError::StreamConfigs)?;
-
     let config = configs
         .filter(|config| config.sample_format() == SAMPLE_FORMAT)
         .filter(|config| config.channels() == bark_protocol::CHANNELS.0)
@@ -17,16 +14,16 @@ pub fn make_stream_config(configs: Box<dyn Iterator<Item = SupportedStreamConfig
 
     let buffer_size = match config.buffer_size() {
         SupportedBufferSize::Range { min, .. } => {
-            std::cmp::max(*min, bark_protocol::FRAMES_PER_PACKET as u32)
+            std::cmp::max(min, &(bark_protocol::FRAMES_PER_PACKET as u32))
         }
         SupportedBufferSize::Unknown => {
-            bark_protocol::FRAMES_PER_PACKET as u32
+            &(bark_protocol::FRAMES_PER_PACKET as u32)
         }
     };
 
     Ok(StreamConfig {
         channels: bark_protocol::CHANNELS.0,
         sample_rate: cpal::SampleRate(bark_protocol::SAMPLE_RATE.0),
-        buffer_size: BufferSize::Fixed(buffer_size),
+        buffer_size: BufferSize::Fixed(*buffer_size),
     })
 }
