dcb43fa7d466c6aba09b63acc885ba1e43473f54
ckie <git-525ff67@ckie.dev>
2024-04-14 20:36:42 +0300

    rip out multicast; not that desperate for it

--- bark/src/socket.rs
+++ bark/src/socket.rs
@@ -92,15 +92,15 @@ fn open_multicast(group: Ipv4Addr, bind: SocketAddrV4) -> Result<socket2::Socket
     let socket = bind_socket(bind)?;
 
     // join multicast group
-    socket
-        .join_multicast_v4(&group, &Ipv4Addr::UNSPECIFIED)
-        .map_err(|e| ListenError::JoinMulticastGroup(group, e))?;
+    // socket
+    //     .join_multicast_v4(&group, &Ipv4Addr::UNSPECIFIED)
+    //     .map_err(|e| ListenError::JoinMulticastGroup(group, e))?;
 
     // set opts
-    socket
-        .set_broadcast(true)
-        .map_err(ListenError::SetBroadcast)?;
-    let _ = socket.set_multicast_loop_v4(true);
+    // socket
+    //     .set_broadcast(true)
+    //     .map_err(ListenError::SetBroadcast)?;
+    // let _ = socket.set_multicast_loop_v4(true);
 
     Ok(socket.into())
 }
