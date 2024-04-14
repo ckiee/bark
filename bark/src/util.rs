d65cafe696c43d81c0753e4f85f044e95890a330
ckie <git-525ff67@ckie.dev>
2024-04-14 20:06:26 +0300

    that?

--- bark/src/util.rs
+++ bark/src/util.rs
@@ -8,7 +8,7 @@ pub const SAMPLE_FORMAT: SampleFormat = SampleFormat::F32;
 pub fn make_stream_config(configs: Box<dyn Iterator<Item = SupportedStreamConfigRange>>) -> Result<StreamConfig, RunError> {
     let config = configs
         .filter(|config| config.sample_format() == SAMPLE_FORMAT)
-        .filter(|config| config.channels() == bark_protocol::CHANNELS.0)
+        // .filter(|config| config.channels() == bark_protocol::CHANNELS.0)
         .nth(0)
         .ok_or(RunError::NoSupportedStreamConfig)?;
 
